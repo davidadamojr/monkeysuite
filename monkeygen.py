6d63c2293075a32730b7a3186c1128bfa7164f9b
David Adamo Jr. <me@davidadamojr.com>
2016-04-12 09:36:49 -0500

    implemented code coverage collection

--- monkeygen.py
+++ monkeygen.py
@@ -2,14 +2,78 @@ import sys
 import random
 import time
 import os
+import subprocess
+import datetime
 
 TEST_SUITE_DIR = "testsuites"
 AUT_PACKAGE_NAME = "org.tomdroid"
 SDK_PATH = "/home/davidadamojr/Android/Sdk" 
+INSTRUMENTATION_CLASS = "com.davidadamojr.tester.EmmaInstrumentation"
+DEVICE_COV_PATH = "/mnt/sdcard/org.tomdroid/coverage.ec"
 
 def print_usage():
     print "Usage: python monkeygen.py generate [no_of_testcases] [no_of_events]\n python monkeygen.py replay [path_to_suite]"
+
+def create_folders():
+    current_time = int(round(time.time() * 1000))
+    suite_folder = str(current_time) + "_suite"
+    coverage_folder = str(current_time) + "_coverage"
+    suite_path = os.path.join(TEST_SUITE_DIR, AUT_PACKAGE_NAME, suite_folder)
+    coverage_path = os.path.join(TEST_SUITE_DIR, AUT_PACKAGE_NAME, coverage_folder)
+    if not os.path.isfile(suite_path):
+        os.makedirs(suite_path)
     
+    if not os.path.isfile(suite_path):
+        os.makedirs(coverage_path)
+    
+    return (suite_path, coverage_path)
+
+def collect_coverage():
+    pass
+        
+def generate_testcases(paths):
+    suite_path = paths[0]
+    cov_path = paths[1]
+    with open(os.path.join(suite_path, "suite.monkey"), 'w') as suite_file:
+        existing_nums = set([])
+        adb_path = SDK_PATH + "/platform-tools/adb"
+        clear_command = adb_path + " shell pm clear " + AUT_PACKAGE_NAME
+        for i in range(0, no_of_testcases):
+            # clear package data
+            subprocess.call(clear_command, shell=True)
+            rand_num = random.randint(0, no_of_testcases)
+            while rand_num in existing_nums:
+                rand_num = random.randint(0, no_of_testcases*2)
+            
+            existing_nums.add(rand_num)
+            
+            # launch app with instrumentation
+            app_launch_cmd = adb_path + " shell am instrument -e coverage true " + \
+                            "-w " + AUT_PACKAGE_NAME + "/" + INSTRUMENTATION_CLASS
+            launched_app = subprocess.Popen(app_launch_cmd, shell=True)
+            
+            """
+            monkey_options = "-v " + str(no_of_events) + \
+                            " -s " + str(rand_num) + " --throttle 10000"
+            """
+            monkey_options = "-p " + AUT_PACKAGE_NAME + " -v " + str(no_of_events) + \
+                            " -s " + str(rand_num) + " --throttle 10000"   
+            monkey_command = adb_path + " shell monkey " + monkey_options
+            subprocess.call(monkey_command, shell=True)     # waits for command to complete
+            suite_file.write(str(rand_num) + os.linesep)
+            
+            # end code coverage collection
+            end_broadcast_cmd = adb_path + " shell am broadcast -a com.davidadamojr.tester.finishtesting"
+            subprocess.call(end_broadcast_cmd, shell=True)
+            
+            cov_filename = "coverage" + str(i) + ".ec"
+            renamed_local_cov_path = os.path.join(cov_path, cov_filename)
+            file_pull_cmd = adb_path + " pull " + DEVICE_COV_PATH + " " + cov_path
+            subprocess.call(file_pull_cmd, shell=True)
+    
+            rename_cmd = "mv " + os.path.join(cov_path, "coverage.ec") + " " + renamed_local_cov_path
+            subprocess.call(rename_cmd, shell=True) 
+       
 if len(sys.argv) < 3:
     print_usage()
 else:
@@ -25,27 +89,8 @@ else:
                 print_usage()
                 sys.exit(0)
             
-            suite_name = time.time() + "_suite.monkey"
-            suite_path = os.path.join(TEST_SUITE_DIR, suite_name)
-            if not os.path.isfile(suite_path):
-                os.makedirs(suite_path)
-            
-            existing_nums = set([])
-            with open(suite_path) as suite_file:
-                rand_num = random.randint(0, no_of_testcases)
-                while rand_num in existing_nums:
-                    rand_num = random.randint(0, no_of_testcases*2)
-                
-                existing_nums.add(rand_num)
-                adb_path = SDK_PATH + "/platform-tools/adb"
-                monkey_options = "-p " + AUT_PACKAGE_NAME + " -v " + no_of_events + " -s " + rand_num
-                monkey_command = adb_path + " shell monkey " + monkey_options
-            
-            
-            
-        
-            
-        
+            paths = create_folders()
+            generate_testcases(paths)
     elif task == "replay":
         pass
     else:
